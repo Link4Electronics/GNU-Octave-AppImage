cd550c5e21b856ceffa54fa5dc72e91ca7026253
Link4Electronics <link4electronics@gmail.com>
2026-04-05 22:40:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/octave.png
-export DESKTOP=/usr/share/applications/org.octave.Octave.desktop
 export DEPLOY_QT=1
 export QT_DIR=qt6
 
